53d53a5d630ac455608cef4cf9ae0a31d3f6dd21
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2024-10-05 11:51:01 +0300

    Revert "2 create header for logged user (#4)"
    This reverts commit cc03bf276ea482af1b3fae782502248b269e6e0b.

--- src/app/private/pages/home/home.component.ts
+++ src/app/private/pages/home/home.component.ts
@@ -1,10 +1,8 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { UserService } from '../../../shared/providers/services/user.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent{
-}
+export class HomeComponent {}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,5 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {AuthService} from "../../providers/services/auth.service";
-import { Router } from '@angular/router';
-import { UserService } from '../../providers/services/user.service';
-import { User } from '../../interfaces/user.interfaces';
 
 @Component({
   selector: 'app-header',
@@ -10,36 +7,6 @@ import { User } from '../../interfaces/user.interfaces';
   styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
   auth = inject(AuthService);
-  #router = inject(Router);
-  userService = inject(UserService);
-  user: User | null = null ;
-  isShowForm: boolean = false;
-
-
-
-  ngOnInit() {
-    this.userService.user$.subscribe({
-      next: (user) => {
-        this.user = user;
-      }
-    });
-    this.userService.isShowForm$.subscribe((value) => {
-      this.isShowForm = value;
-    });
-
-  }
-
-  isShowFormEdit() {
-    this.isShowForm = true;
-    this.userService.setShowForm(this.isShowForm);
-  }
-
-  logout(event: Event) {
-    event.preventDefault();
-    this.auth.logout();
-    this.#router.navigate(['login'])
-  }
-
 }
