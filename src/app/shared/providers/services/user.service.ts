9cbcf60340bab94d518d04c29a14da7326624eff
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2024-10-15 10:35:35 +0300

    add review changes

--- src/app/shared/providers/services/user.service.ts
+++ src/app/shared/providers/services/user.service.ts
@@ -33,7 +33,7 @@ export class UserService {
 
   clearUser(): void {
     this.user = null;
-    localStorage.setItem('userId', 'null');
+    localStorage.removeItem('userId');
   }
 
   getUser(): Observable<User | null> {
