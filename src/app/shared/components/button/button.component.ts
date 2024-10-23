c1e53a250bd033c9bc8ca18c4d9b2e6a5e1c7da9
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2024-10-23 11:57:29 +0300

    added review changes for button component

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -7,8 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() isLink: boolean = false;
-  @Input() disabled: boolean = true;
-  @Input() label: string = '';
+  @Input() disabled: boolean = false;
   @Input() linkUrl: string = '';
   @Input() type: string = '';
   @Input() classes: string = '';
