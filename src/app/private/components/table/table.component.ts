73f2bcc202968398844152f62e6a06e2bea6fd71
Svetlana Nagovicyna <52661934+SvetlanaNagovicyna@users.noreply.github.com>
2024-12-28 18:01:06 +0200

    moved icon registry to app component

--- src/app/private/components/table/table.component.ts
+++ src/app/private/components/table/table.component.ts
@@ -1,7 +1,5 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DailyRecord } from '../../../shared/interfaces/daily-record';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-table',
@@ -10,17 +8,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 
 export class TableComponent {
-  iconRegistry: MatIconRegistry = inject(MatIconRegistry);
-  domSanitizer: DomSanitizer = inject(DomSanitizer);
   @Input() monthRecord: DailyRecord[] = [];
 
   columnsToDisplayHeader: string[] = ['date', 'came', 'leave', 'summary', 'result'];
   columnsToDisplayFooter: string[] = ['work', 'total', 'resultFooter'];
-
-  constructor() {
-    this.iconRegistry.addSvgIconSet(
-      this.domSanitizer.bypassSecurityTrustResourceUrl("/assets/img/icons.svg")
-    );
-  }
-
 }
